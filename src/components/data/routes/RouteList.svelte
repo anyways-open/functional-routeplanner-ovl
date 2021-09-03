a4378e4caf25db9399810117eb9f1df1b6301d9d
xivk <ben@anyways.eu>
2021-09-03 14:35:24 +0200

    Only show one route when multiple segments.

--- src/components/data/routes/RouteList.svelte
+++ src/components/data/routes/RouteList.svelte
@@ -1,31 +1,42 @@
 <script lang="ts">
-import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher } from "svelte";
 
     import type { Route } from "../Route";
     import RouteRow from "./RouteRow.svelte";
 
     export let routes: Route[] = [];
     export let selected: number = -1;
+    export let maxAlternatives: number = 1;
 
     const dispatch = createEventDispatcher<{ select: number }>();
     function onSelect(i: number): void {
         dispatch("select", i);
     }
+
+    $: if (typeof routes !== "undefined") {
+        maxAlternatives = 1;
+        if (
+            typeof routes[0] !== "undefined" &&
+            typeof routes[0].segments !== "undefined"
+        ) {
+            maxAlternatives = routes[0].segments.length > 1 ? 1 : routes.length;
+        }
+    }
 </script>
 
-{#if routes.length > 0 && routes.findIndex(r => typeof r !== "undefined" && typeof r.segments !== "undefined" && r.segments.length > 0) >= 0}
+{#if routes.length > 0 && routes.findIndex((r) => typeof r !== "undefined" && typeof r.segments !== "undefined" && r.segments.length > 0) >= 0}
     <div>
         <div class="route-results">
-            <div class="d-block d-sm-none"> 
-                Routes
-            </div>
+            <div class="d-block d-sm-none">Routes</div>
             <div class="route-results-list">
                 {#each routes as route, i}
-                {#if typeof route !== "undefined" && 
-                    typeof route.segments !== "undefined" &&
-                    route.segments.length > 0}
-                    <RouteRow {route} selected={selected == i} on:select={() => onSelect(i)} />
-                {/if}
+                    {#if typeof route !== "undefined" && typeof route.segments !== "undefined" && route.segments.length > 0 && i < maxAlternatives}
+                        <RouteRow
+                            {route}
+                            selected={selected == i}
+                            on:select={() => onSelect(i)}
+                        />
+                    {/if}
                 {/each}
             </div>
         </div>
@@ -47,7 +58,7 @@ import { createEventDispatcher } from "svelte";
         width: 100%;
     }
 
-    @media (min-width: 576px) { 
+    @media (min-width: 576px) {
         .route-results-list {
             margin-top: 0px;
             border-radius: unset;
