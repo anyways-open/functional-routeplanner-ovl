5d43f987acb93e9c3871711d730256010f7cf1fc
xivk <ben@anyways.eu>
2021-09-22 12:43:54 +0200

    Added bidirectional arrows.

--- src/components/map/Map.svelte
+++ src/components/map/Map.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
     import { onMount, setContext } from "svelte";
-    import { FullscreenControl, LngLatLike, Map, NavigationControl } from "mapbox-gl";
+    import {
+        FullscreenControl,
+        LngLatLike,
+        Map,
+        NavigationControl,
+    } from "mapbox-gl";
     import "../../../node_modules/mapbox-gl/dist/mapbox-gl.css";
     import { key } from "./map";
     import { MapHook } from "./MapHook";
     import { UrlHashHandler } from "../../shared/UrlHashHandler";
-    
+
     // exports.
     export let hook: MapHook = new MapHook();
 
@@ -16,11 +21,10 @@
     // get map context.
     setContext(key, {
         getMap: () => {
-            return { map: map, hook: hook};
-        }
+            return { map: map, hook: hook };
+        },
     });
     onMount(async () => {
-
         let center: LngLatLike = [3.74475, 51.04774];
         let zoom: number = 10.51;
         const mapState = urlHash.getState();
@@ -34,13 +38,30 @@
         }
 
         const styleResponse = await fetch(
-                //"https://api.maptiler.com/maps/152a1435-6dc1-441e-be13-3647c1ccb483/style.json?key=OZUCIh4RNx38vXF8gF4H"
-                "https://api.maptiler.com/maps/5ee3edf5-df11-4b36-88c9-f660f7afded9/style.json?key=OZUCIh4RNx38vXF8gF4H"
-            );
+            "https://api.maptiler.com/maps/152a1435-6dc1-441e-be13-3647c1ccb483/style.json?key=OZUCIh4RNx38vXF8gF4H"
+            //"https://api.maptiler.com/maps/5ee3edf5-df11-4b36-88c9-f660f7afded9/style.json?key=OZUCIh4RNx38vXF8gF4H"
+        );
         const styleJson = await styleResponse.json();
+
+        // styleJson.layers.push({
+        //     id: "bicycle-oneway",
+        //     source: "openmaptiles",
+        //     "source-layer": "transportation",
+        //     type: "line",
+        //     paint: {
+        //         "line-color": "#f00",
+        //         "line-width": 1,
+        //     },
+        //     layout: {
+        //         "line-cap": "round",
+        //         "line-join": "round",
+        //     },
+        //     filter: ["all", ["==", "oneway:bicycle", "no"]],
+        // });
         // styleJson.sources.openmaptiles.url = "https://staging.anyways.eu/openmaptiles/data/v3.json";
         //styleJson.sources.openmaptiles.url = "https://tiles.anyways.eu/data/v3.json";
-        styleJson.sources.openmaptiles.url = "https://api.anyways.eu/tiles/openmaptiles/mvt.json";
+        styleJson.sources.openmaptiles.url =
+            "https://api.anyways.eu/tiles/openmaptiles/mvt.json";
 
         map = new Map({
             container: "mapbox-gl-container",
@@ -48,13 +69,11 @@
             center: center,
             zoom: zoom,
             hash: false,
-            attributionControl: false
+            attributionControl: false,
         });
 
-
-
         const nav = new NavigationControl({
-            visualizePitch: true
+            visualizePitch: true,
         });
         map.addControl(nav, "top-right");
 
@@ -62,11 +81,39 @@
 
         map.on("move", () => {
             const center = map.getCenter();
-            urlHash.update(`${map.getZoom().toFixed(2)}/${center.lng.toFixed(5)}/${center.lat.toFixed(5)}`);
+            urlHash.update(
+                `${map.getZoom().toFixed(2)}/${center.lng.toFixed(
+                    5
+                )}/${center.lat.toFixed(5)}`
+            );
         });
 
         map.on("load", () => {
             map.resize(); // on more resize, refresh on chrome broken.
+
+            map.loadImage("assets/img/icons/double-arrow-8.png", (e, i) => {
+                if (e) throw e;
+
+                map.addImage("double-arrow", i);
+                map.addLayer({
+                    id: "bicycle-oneway-arrows",
+                    type: "symbol",
+                    source: "openmaptiles",
+                    "source-layer": "transportation",
+                    minzoom: 9,
+                    paint: { "icon-opacity": 0.3 },
+                    layout: {
+                        "icon-image": "double-arrow",
+                        "icon-rotate": 0,
+                        "icon-padding": 10,
+                        "icon-size": 0.75,
+                        "symbol-spacing": 150,
+                        "symbol-placement": "line",
+                        "icon-rotation-alignment": "map",
+                    },
+                    filter: ["all", ["==", "oneway:bicycle", "no"]],
+                });
+            });
         });
 
         hook.resize = () => {
@@ -75,7 +122,7 @@
         hook.flyTo = (center) => {
             map.flyTo({
                 center: [center.lng, center.lat],
-                minZoom: 15
+                minZoom: 15,
             });
         };
         hook.on = (name, handler) => {
@@ -86,7 +133,7 @@
 
 <div id="mapbox-gl-container" class="h-100 map">
     {#if map}
-    <slot></slot>
+        <slot />
     {/if}
 </div>
 
