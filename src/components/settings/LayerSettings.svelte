a9f925b276597da668f717a7298034163d0b815f
xivk <ben@anyways.eu>
2021-12-29 10:29:37 +0100

    Added legenda.

--- src/components/settings/LayerSettings.svelte
+++ src/components/settings/LayerSettings.svelte
@@ -85,6 +85,88 @@
 
 <h2 class="mt-3 text-center">Kaartlagen</h2>
 
+<h3 class="pt-2">Basiskaart</h3>
+
+<p class="pb-2">
+    De basiskaart, gebaseerd op OpenStreetMap (OSM), toont specifiek informatie
+    gericht op fietsers. De volgende informatie is op de kaart zichtbaar:
+</p>
+
+<div class="card p-2 m-2">
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <div class="fietsstraat" />
+        </div>
+        <div class="col">
+            Een fietsstraat, fietsers hebben hier voorrang en mogen niet
+            voorbijgestoken worden.
+        </div>
+    </div>
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <div class="fietspad" />
+        </div>
+        <div class="col">
+            Volledig vrijliggend fietspad enkel voor fietsers en some
+            voetgangers.
+        </div>
+    </div>
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <div class="fietspad-aanliggend" />
+        </div>
+        <div class="col">
+            Aanliggend fietspad, een fietspad dat deel is van de rijbaan voor
+            auto verkeer.
+        </div>
+    </div>
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <img
+                class="eenrichting-fietsers"
+                src="assets/img/icons/double-arrow-8.png"
+                alt="Eenrichtingspijltjes" />
+        </div>
+        <div class="col">
+            Een eenrichtingsstraat waar fietsers in beide richtingen mogen
+            fietsen.
+        </div>
+    </div>
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <img
+                class="eenrichting"
+                src="assets/img/icons/arrow-8.png"
+                alt="Eenrichtingspijltjes" />
+        </div>
+        <div class="col">
+            Een normale eenrichtingsstraat, ook geldig voor fietsers.
+        </div>
+    </div>
+
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <img
+                class="bicycle-parking"
+                src="assets/img/icons/bicycle-parking-16.png"
+                alt="Fietsparking" />
+        </div>
+        <div class="col">Fietsenrekken of een fietsparking.</div>
+    </div>
+
+    <div class="row align-items-center px-0 py-2">
+        <div class="col-3 ms-3 legenda-item">
+            <img
+                class="bicycle-shop"
+                src="assets/img/icons/bicycle-shop-32.png"
+                alt="Fietswinkel" />
+        </div>
+        <div class="col">Een fietswinkel.</div>
+    </div>
+</div>
+
+<h3 class="pt-2">Lagen</h3>
+
 <div class="row m-2 p-2">De volgende lagen zijn beschikbaar:</div>
 
 <div class="card p-2 m-2">
@@ -95,11 +177,12 @@
                     <button
                         class="btn {layer.visible ? 'active' : ''} border-0"
                         type="button"
-                        on:click={() => onLayerToggle(i)}
-                    >
+                        on:click={() => onLayerToggle(i)}>
                         <div class="button-content">
                             <span>
-                                <div class="img-container"><img src={layer.logo} alt={layer.name} /></div>
+                                <div class="img-container">
+                                    <img src={layer.logo} alt={layer.name} />
+                                </div>
                             </span>
                             <span>
                                 {layer.name}
@@ -161,5 +244,51 @@
 
     .img-container {
         width: 100%;
+    }
+
+    .legenda-item {
+        height: 20px;
+        background-color: white;
+        border-radius: 0.25rem;
+    }
+
+    .fietsstraat {
+        border-top: #0000ff85;
+        border-top-style: solid;
+        border-top-width: 6px;
+        width: 100%;
+        margin-top: 7px;
+    }
+
+    .fietspad {
+        border-top: #0000ff85;
+        border-top-style: solid;
+        border-top-width: 3px;
+        height: 3px;
+        width: 100%;
+        margin-top: 9px;
+    }
+
+    .fietspad-aanliggend {
+        border-top: #0000ff85;
+        border-top-style: dotted;
+        border-top-width: 3px;
+        width: 100%;
+        margin-top: 9px;
+    }
+
+    .eenrichting-fietsers {
+        margin-left: 17px;
+    }
+
+    .eenrichting {
+        margin-left: 20px;
+    }
+    .bicycle-parking {
+        margin-left: 20px;
+    }
+    .bicycle-shop {
+        margin-left: 20px;
+        height: 16px;
     }
 </style>
