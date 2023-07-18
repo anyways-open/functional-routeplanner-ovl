bd0c187536a7bb87a48b9d94a332a860322a1b28
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-07-18 15:14:13 +0200

    Update to new tile URL

--- src/components/map/layers/ImageryLayer.svelte
+++ src/components/map/layers/ImageryLayer.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import type { Map } from "mapbox-gl";
-    import { onMount, getContext } from "svelte";
-    import { key } from "../../map/map";
+    import type {Map} from "mapbox-gl";
+    import {getContext, onMount} from "svelte";
+    import {key} from "../../map/map";
 
-    const { getMap } = getContext(key);
+    const {getMap} = getContext(key);
     const map: Map = getMap().map;
 
     onMount(async () => {
@@ -11,7 +11,7 @@
             map.addSource("aiv", {
                 type: "raster",
                 tiles: [
-                    "https://tile.informatievlaanderen.be/ws/raadpleegdiensten/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=omwrgbmrvl&STYLE=&FORMAT=image/png&tileMatrixSet=GoogleMapsVL&tileMatrix={z}&tileRow={y}&tileCol={x}",
+                    "https://geo.api.vlaanderen.be/OFW/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ofw&STYLE=&FORMAT=image/png&tileMatrixSet=GoogleMapsVL&tileMatrix={z}&tileRow={y}&tileCol={x}"
                 ],
                 tileSize: 256,
                 attribution: "AIV",
