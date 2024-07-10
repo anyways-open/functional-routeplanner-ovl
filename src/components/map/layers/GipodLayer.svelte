739c1d2b07b552ec24263ae749f381ccd94b7cac
xivk <ben@anyways.eu>
2024-07-10 09:49:25 +0200

    Attempt to fix gipod layers.

--- src/components/map/layers/GipodLayer.svelte
+++ src/components/map/layers/GipodLayer.svelte
@@ -41,15 +41,18 @@
             map.addSource("gipod-con", {
                 type: "raster",
                 tiles: [
-                    "https://geoservices.informatievlaanderen.be/raadpleegdiensten/gipodpubliek/wms?SERVICE=WMS&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&STYLES=default&VERSION=1.3.0&LAYERS=WoCon&WIDTH=1905&HEIGHT=303&CRS=EPSG:3857&BBOX={bbox-epsg-3857}",
+                    //"https://geo.api.vlaanderen.be/gipod/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&layers=GIPOD_WERK_VNDG&STYLES=default&CRS=EPSG%3A31370&WIDTH=1918&HEIGHT=539&BBOX={bbox-epsg-3857}"
+                    "https://geo.api.vlaanderen.be/gipod/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&layers=GIPOD_WERK_VNDG&STYLES=default&&WIDTH=1905&HEIGHT=303&CRS=EPSG:3857&BBOX={bbox-epsg-3857}",
                 ],
                 tileSize: 256,
             });
+            console.log("gipod-con");
 
             map.addSource("gipod-icon", {
                 type: "raster",
                 tiles: [
-                    "https://geoservices.informatievlaanderen.be/raadpleegdiensten/gipodpubliek/wms?SERVICE=WMS&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&STYLES=default&VERSION=1.3.0&LAYERS=WoIcoon&WIDTH=1905&HEIGHT=303&CRS=EPSG:3857&BBOX={bbox-epsg-3857}",
+                    //"https://geo.api.vlaanderen.be/gipod/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&layers=GIPOD_WERK_VNDG_ICOON&STYLES=default&CRS=EPSG%3A31370&WIDTH=1918&HEIGHT=598&BBOX=93855.55212791222%2C208119.68074417542%2C94335.05212791222%2C208269.18074417542"
+                    "https://geo.api.vlaanderen.be/gipod/wms?SERVICE=WMS&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&STYLES=default&VERSION=1.3.0&LAYERS=GIPOD_WERK_VNDG_ICOON&WIDTH=1905&HEIGHT=303&CRS=EPSG:3857&BBOX={bbox-epsg-3857}",
                 ],
                 tileSize: 256,
             });
